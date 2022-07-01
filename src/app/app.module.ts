78a09e2580f14de46a27f72fdee9f741620de98e
Nikhil <107390052+Nikhilyadav777@users.noreply.github.com>
2022-07-01 10:31:10 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BusinessNewsComponent } from './business-news/business-news.component';
+import { HeaderComponent } from './header/header.component';
+import { HealthNewsComponent } from './health-news/health-news.component';
+import { PoliticsNewsComponent } from './politics-news/politics-news.component';
+import { SportNewsComponent } from './sport-news/sport-news.component';
+import { TechnewsComponent } from './technews/technews.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SportNewsComponent,
+    TechnewsComponent,
+    BusinessNewsComponent,
+    HeaderComponent,
+    PoliticsNewsComponent,
+    HealthNewsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
