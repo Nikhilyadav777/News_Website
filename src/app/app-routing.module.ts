d3086e6a91ea4addbf574e448fa8c1d19105fd79
Nikhil <107390052+Nikhilyadav777@users.noreply.github.com>
2022-07-01 10:28:03 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BusinessNewsComponent } from './business-news/business-news.component';
+import { HeaderComponent } from './header/header.component';
+import { HealthNewsComponent } from './health-news/health-news.component';
+import { PoliticsNewsComponent } from './politics-news/politics-news.component';
+import { SportNewsComponent } from './sport-news/sport-news.component';
+import { TechnewsComponent } from './technews/technews.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HeaderComponent},
+  {path:"politics", component:PoliticsNewsComponent},
+  {path:"sport", component:SportNewsComponent},
+  {path:"tech", component:TechnewsComponent},
+  {path:"health", component:HealthNewsComponent},
+  {path:"business", component:BusinessNewsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
